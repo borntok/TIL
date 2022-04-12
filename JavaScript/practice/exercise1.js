10167661a194fc53e9e27538159454c2e07fecc2
JU Yerin <yerin0823@naver.com>
2022-04-12 14:27:02 +0900

    반복문 연습문제

---
+++ JavaScript/practice/exercise1.js
@@ -0,0 +1,19 @@
+// 반복문 for를 이용하여 0부터 10까지의 정수 중 짝수의 합을 구한 뒤 출력해주는 코드를 작성하시오.
+
+const UNTIL_NUM = 10;
+let sum = 0;
+
+for (i = 0; i <= UNTIL_NUM; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+console.log(sum);
+
+sum = 0;
+for (i = UNTIL_NUM; i >= 0; i--) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+console.log(sum);
