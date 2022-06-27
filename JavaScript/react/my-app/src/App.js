bc9955ab170c5a5c8bfac9ad95a403f4dcd013a6
JU Yerin <yerin0823@naver.com>
2022-06-27 14:40:00 +0900

    명함 추첨

--- JavaScript/react/my-app/src/App.js
+++ JavaScript/react/my-app/src/App.js
@@ -1,7 +1,42 @@
-import FunctionClock from "./components/FunctionClock";
+import React, { useEffect, useState } from "react";
+import datas from "./data/cards";
+import BusinessCard from "./components/BusinessCard";
 
+// 추첨하기 버튼과 명함 컴포넌트 구현
 function App() {
-  return <FunctionClock />;
+  const [cards, setCards] = useState([]);
+  const [pickedCards, setPickedCards] = useState([]);
+
+  function draw() {
+    if (pickedCards.length > 2) {
+      const names = pickedCards.reduce((acc, cur) => {
+        return (acc = acc.concat(`${cur.name}, `));
+      }, "");
+      alert(`당첨자는 ${names} 입니다.`);
+    }
+
+    // 추첨하기 버튼을 누르면, 랜덤하게 하나의 명함을 선택
+    const randomIdx = Math.floor(Math.random() * cards.length);
+    const randomItem = cards[randomIdx];
+
+    // 중복 제거
+    setCards(cards.filter((c) => c.phoneNumber !== randomItem.phoneNumber));
+    // 당첨자(array) 관리
+    setPickedCards([...pickedCards, randomItem]);
+  }
+
+  useEffect(() => {
+    setCards(datas);
+  }, []);
+
+  return (
+    <div>
+      {cards.length > 0 && <button onClick={draw}>추첨하기</button>}
+      {pickedCards.length > 0 && (
+        <BusinessCard info={pickedCards[pickedCards.length - 1]} />
+      )}
+    </div>
+  );
 }
 
 export default App;
