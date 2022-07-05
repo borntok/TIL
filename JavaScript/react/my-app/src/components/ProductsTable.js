61928959a598f6b5eb212dac72c8c01837b58628
JU Yerin <yerin0823@naver.com>
2022-07-05 09:19:07 +0900

    styled-components

--- JavaScript/react/my-app/src/components/ProductsTable.js
+++ JavaScript/react/my-app/src/components/ProductsTable.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import * as S from "./ProductsTable.style";
 
 export default function ProductsTable(props) {
   const { category, items, inStockOnly } = props;
@@ -8,15 +10,19 @@ export default function ProductsTable(props) {
 
   return (
     <>
-      <tr style={{ fontWeight: "bold" }}>
-        <td>{category}</td>
+      <tr>
+        <S.Category>{category}</S.Category>
       </tr>
       {filteredItems.map((item, idx) => (
         <tr key={idx}>
-          <td style={{ color: item.stocked ? "black" : "red" }}>{item.name}</td>
+          <ProductName stocked={item.stocked}>{item.name}</ProductName>
           <td>{item.price}</td>
         </tr>
       ))}
     </>
   );
 }
+
+const ProductName = styled.td`
+  color: ${(props) => (props.stocked ? "black" : "red")};
+`;
