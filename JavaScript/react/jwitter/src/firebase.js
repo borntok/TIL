f08de2d99e57ea5b33fba763df5a48089fff3260
JU Yerin <yerin0823@naver.com>
2022-07-09 08:50:29 +0900

    setting

---
+++ JavaScript/react/jwitter/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2tGaISwsWB1jHd1n4RuNx2YaR-AXHqfg",
+  authDomain: "jwitter-cf8c5.firebaseapp.com",
+  projectId: "jwitter-cf8c5",
+  storageBucket: "jwitter-cf8c5.appspot.com",
+  messagingSenderId: "671956957601",
+  appId: "1:671956957601:web:67367195223cca5bbf33e0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
