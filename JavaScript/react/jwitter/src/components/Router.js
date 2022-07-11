0ea2b01f8125efadfe3ee77c35d05323b198feab
JU Yerin <yerin0823@naver.com>
2022-07-11 17:47:43 +0900

    login form

--- JavaScript/react/jwitter/src/components/Router.js
+++ JavaScript/react/jwitter/src/components/Router.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-export default function AppRouter() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+export default function AppRouter(props) {
   return (
     <BrowserRouter>
       <Routes>
-        {isLoggedIn ? (
+        {props.isLoggedIn ? (
           <>
             <Route path={"/"} element={<Home />} />
           </>
