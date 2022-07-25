67d2c4af0084eb4a811a0f24a5bc9f2f15fb17c7
JU Yerin <yerin0823@naver.com>
2022-07-25 15:39:29 +0900

    swr

--- JavaScript/react/my-app/src/blog/ReactPage.js
+++ JavaScript/react/my-app/src/blog/ReactPage.js
@@ -1,56 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import axios from "axios";
+import useSWR from "swr";
 
 export default function ReactPage() {
   // api call -> react에 해당하는 글의 목록을 응답 받음.
-  const [docs, setDocs] = useState([]);
+  const [number, setNumber] = useState(0);
 
-  // const docs = [
-  //   {
-  //     id: 1,
-  //     title: "JSX",
-  //     date: "07/04/2022",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "components",
-  //     date: "07/05/2022",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "state",
-  //     date: "07/06/2022",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "hook",
-  //     date: "07/06/2022",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "styled component",
-  //     date: "07/07/2022",
-  //   },
-  // ];
+  async function fetcher(url) {
+    const result = await axios.get(url);
 
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      console.log(result);
-      console.log(result.data);
-      return result.data;
-    }
+    console.log(result.data);
+    return result.data;
+  }
 
-    fetchData().then((res) => {
-      setDocs(res);
-    });
-  }, []);
+  const { data: docs, error } = useSWR("posts", () =>
+    fetcher("https://jsonplaceholder.typicode.com/posts")
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!docs) return <div>loading...</div>;
 
   return (
     <div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
       {docs.map((doc) => (
         <Link
           to={`${doc.id}`}
