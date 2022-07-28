e7abf001e55421f36c2530298ec38e0846e515b5
JU Yerin <yerin0823@naver.com>
2022-07-29 08:54:10 +0900

    react-query mutation

--- JavaScript/react/my-app/src/pages/Edit.js
+++ JavaScript/react/my-app/src/pages/Edit.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "../mocks/api";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getUser, updateNickname } from "../mocks/api";
 
 // api를 통해 현재 닉네임값 가져오기
 // handleSubmit: 업데이트된 inputValue를 서버에 전송
 
 export default function Edit() {
   const [inputValue, setInputValue] = useState("");
-
-  const { data, isLoading } = useQuery(["getUser"], getUser);
+  const queryClient = useQueryClient();
+  const { data, isLoading } = useQuery(["@getUser"], getUser);
+  const mutation = useMutation(updateNickname, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["@getUser"]);
+    },
+  });
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    mutation.mutate(inputValue);
   };
 
   if (isLoading) return <span>Loading...</span>;
