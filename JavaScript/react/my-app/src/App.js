4bab5f552b7995473055d42537f2d112abaadb21
JU Yerin <yerin0823@naver.com>
2022-07-29 08:30:10 +0900

    react-query queries

--- JavaScript/react/my-app/src/App.js
+++ JavaScript/react/my-app/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
--- JavaScript/react/my-app/src/pages/Edit.js
+++ JavaScript/react/my-app/src/pages/Edit.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../mocks/api";
 
 // api를 통해 현재 닉네임값 가져오기
 // handleSubmit: 업데이트된 inputValue를 서버에 전송
 
 export default function Edit() {
   const [inputValue, setInputValue] = useState("");
+
+  const { data, isLoading } = useQuery(["getUser"], getUser);
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
@@ -13,10 +17,12 @@ export default function Edit() {
     e.preventDefault();
   };
 
+  if (isLoading) return <span>Loading...</span>;
+
   return (
     <>
       <h1>Edit</h1>
-      <h3>현재 닉네임: {"riri"}</h3>
+      <h3>현재 닉네임: {data.nickName}</h3>
       <form onSubmit={handleSubmit}>
         <label>
           변경할 닉네임:
