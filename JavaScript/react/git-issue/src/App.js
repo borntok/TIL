f4144d3b904a4236815b05bde39381db3040a6f5
JU Yerin <yerin0823@naver.com>
2022-09-14 16:02:14 +0900

    page layout

--- JavaScript/react/git-issue/src/App.js
+++ JavaScript/react/git-issue/src/App.js
@@ -1,26 +1,14 @@
-import React, { useEffect, useState } from "react"
-
-// useEffect
-/**
- * mount (= 리액트 컴포넌트가 그려질 때, 렌더될 때)
- * unmount (= 리액트 컴포넌트가 사라질 때, 지워질 때)
- * update (= 특정 값이 변해서 리액트 컴포넌트가 다시 그려질 때)
- */
+import styles from "./App.module.css"
+import React from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    if (count === 5) {
-      console.log("This is five.")
-    }
-  }, [count])
-
   return (
-    <div>
-      <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </div>
+    <>
+      <div className={styles.nav}>Nav</div>
+      <div className={styles.header}>Header</div>
+      <div className={styles.listContainer}>ListContainer</div>
+      <div className={styles.footer}>Footer</div>
+    </>
   )
 }
 
