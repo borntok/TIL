469ffb047b639169b4093046732b3d6ced2e7c91
JU Yerin <yerin0823@naver.com>
2022-09-14 21:55:34 +0900

    filter input

---
+++ JavaScript/react/git-issue/src/ListContainer.js
@@ -0,0 +1,25 @@
+import styles from "./ListContainer.module.css"
+import Button from "./components/Button"
+
+import { useState } from "react"
+
+export default function ListContainer() {
+  const [inputValue, setInputValue] = useState("is:pr is:open")
+
+  return (
+    <div className={styles.listContainer}>
+      <div className={styles.topSection}>
+        <input
+          className={styles.input}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          style={{ fontSize: "14px", backgroundColor: "green", color: "white" }}
+        >
+          New Issue
+        </Button>
+      </div>
+    </div>
+  )
+}
