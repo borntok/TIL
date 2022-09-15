b886e527477a38d168ed122d325ff689fb0d298f
JU Yerin <yerin0823@naver.com>
2022-09-15 11:01:01 +0900

    Badge

--- JavaScript/react/git-issue/src/components/ListItem.js
+++ JavaScript/react/git-issue/src/components/ListItem.js
@@ -1,12 +1,23 @@
 import styles from "./ListItem.module.css"
-import ListItemLayout from "./ListItemLayout"
 
-export default function ListItem({ checked, onChangeCheckBox, onClickTitle }) {
+import ListItemLayout from "./ListItemLayout"
+import Badge from "./Badge"
+
+export default function ListItem({
+  checked,
+  onChangeCheckBox,
+  onClickTitle,
+  badges,
+}) {
   return (
     <ListItemLayout>
       <div>
         <div role="button" onClick={onClickTitle} className={styles.title}>
           Issue Example
+          {badges &&
+            badges.map((badgeProps, idx) => (
+              <Badge key={idx} {...badgeProps} />
+            ))}
         </div>
         <div className={styles.description}># Description</div>
       </div>
