4619b616ca76e4690528b3d3c0ce49a316b74f49
JU Yerin <yerin0823@naver.com>
2022-09-15 14:19:59 +0900

    open closed

--- JavaScript/react/git-issue/src/ListContainer.js
+++ JavaScript/react/git-issue/src/ListContainer.js
@@ -1,4 +1,5 @@
 import styles from "./ListContainer.module.css"
+import cx from "clsx"
 
 import { useState } from "react"
 
@@ -23,6 +24,7 @@ export default function ListContainer() {
           New Issue
         </Button>
       </div>
+      <OpenClosedFilters />
       <ListItemLayout className={styles.listFilter}>
         <div className={styles.filterLists}>
           <span>Author</span>
@@ -44,5 +46,44 @@ export default function ListContainer() {
         />
       </div>
     </div>
+  )
+}
+
+function OpenClosedFilters({ data }) {
+  const [isOpenMode, setIsOpenMode] = useState(true)
+
+  // const data = getData()
+  // const openedData = data.filter((d) => d.state === 'open')
+  // const closedData = data.filter((d) => d.state === 'closed')
+  const openModeDataSize = 1
+  const closeModeDataSize = 2
+
+  return (
+    <>
+      <OpenClosedFilter
+        size={openModeDataSize}
+        state="Open"
+        selected={isOpenMode}
+        onClick={() => setIsOpenMode(true)}
+      />
+      <OpenClosedFilter
+        size={closeModeDataSize}
+        state="Closed"
+        selected={!isOpenMode}
+        onClick={() => setIsOpenMode(false)}
+      />
+    </>
+  )
+}
+
+function OpenClosedFilter({ size, state, onClick, selected }) {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
   )
 }
